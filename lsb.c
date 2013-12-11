8c9f3b2b115900bcc85194c34f17b942d9f22e48
Hitesh Dharmdasani <hitesh.dharamdasani@gmail.com>
2013-12-11 10:08:13 -0800

    Does encrypt/decrypt

--- lsb.c
+++ lsb.c
@@ -6,7 +6,6 @@ int get_image_data_offset(FILE* bmp_offset) {
 	fseek(bmp_offset,10,0);
 	int offset;
 	offset=(int)fgetc(bmp_offset);
-	printf("Image data is at offset = %d",offset);
 	return offset;
 }
 
@@ -16,6 +15,9 @@ int get_message_length(FILE *fp) {
 	fseek(fp, 0L, SEEK_SET);
 	return(size);
 }
+int get_bit(char the_byte,int which_bit) {
+	return((the_byte>>8-which_bit)&1);
+}
 
 int main(int argc,char** argv) {
 	unsigned char mask_table[] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80 };
@@ -24,33 +26,34 @@ int main(int argc,char** argv) {
 	FILE *message_handle;
 	FILE *hidden_message_handle;
 
-	if(argc!=4) {
-		printf("*** Steganography by LSB substitution***\nUsage: %s [-e][-d] [source file] [destination file] [text file]\nMode\n-e : Add text to Image\n-d : Extract text from Image\n",argv[0]);
+	if(argc!=5) {
+		printf("*** Steganography by LSB substitution***\nUsage: %s [-e][-d] <source file> <destination file> <text file>\n-e : Add text to image\n -d : Get text from image\n",argv[0]);
 		exit(1);
 	}
 
+	int mode;
+	if(!strcmp(argv[1],"-e"))
+		mode=1;
+	else if(!strcmp(argv[1],"-d"))
+		mode=0;
+	else {
+		printf("*** Steganography by LSB substitution***\nUsage: %s <mode> <source file> <destination file> <text file>\nMode - e = encrypt \n d= decrypt \n",argv[0]);
+		exit(1);
+	}
 
 	/* HANDLING FILE OPENING AND ERRORS */
-	file_handle=fopen(argv[1],"r");
+	file_handle=fopen(argv[2],"r");
 	if (file_handle == NULL) {
-		fprintf(stderr, "Can't open input file %s\n",argv[1]);
+		fprintf(stderr, "Can't open input file %s\n",argv[2]);
 		exit(1);
 	}
 
-	hidden_message_handle=fopen(argv[2],"w");
+	hidden_message_handle=fopen(argv[3],"w");
 	if (hidden_message_handle== NULL) {
-		fprintf(stderr, "Cannot create output file %s\n",argv[2]);
-		exit(1);
-	}
-		
-	message_handle=fopen(argv[3],"r");
-	if (message_handle== NULL) {
-		fprintf(stderr, "Can't open text input file %s\n",argv[3]);
+		fprintf(stderr, "Cannot create output file %s\n",argv[3]);
 		exit(1);
 	}
 	
-
-	int hidden_message_length=get_message_length(message_handle);
 
 	int c=0;
 
@@ -69,22 +72,33 @@ int main(int argc,char** argv) {
 
 	char file_buffer; 			// Temp variable for one byte from file
 	char message_buffer;		// Temp buffer for one byte of message
-	
+
+	if(mode) {
+		message_handle=fopen(argv[4],"r");
+		if (message_handle== NULL) {
+			fprintf(stderr, "Can't open text input file %s\n",argv[4]);
+			exit(1);
+		}
+		int hidden_message_length=get_message_length(message_handle);
+
+
 	/* 
 	After offset has been read and the file header has been written as is for the virgin image - the length of the hidden message is written as the first byte. This length is then used while decrypting the text from the image. 
 	*/
-	
+		fputc(hidden_message_length,hidden_message_handle);
+		c++;
 		do {
 			int bit_of_message;
 			if(!feof(message_handle)) {		
 				message_buffer=fgetc(message_handle);
-				for(int i=0;i<8;i++) {  //Do this for every bit in every byte of the virgin-image
+				for(int i=1;i<=8;i++) {  //Do this for every bit in every byte of the virgin-image
 
 					file_buffer=fgetc(file_handle);
 					c++;
 					int file_byte_lsb = file_buffer & 1; // AND WITH 1 TO GET THE VALUE OF LSB. AND MAKES IT 0 IF LSB IS 0 OR 1 IF IT IS 1
 
-					bit_of_message=mask_table[i] & message_buffer;
+					bit_of_message=get_bit(message_buffer,i);
+					//bit_of_message=mask_table[i] & message_buffer;
 					if(file_byte_lsb==bit_of_message) {
 						fputc(file_buffer,hidden_message_handle);
 					}
@@ -104,10 +118,31 @@ int main(int argc,char** argv) {
 				c++;
 			}
 		} while(!feof(file_handle));	
+		fclose(message_handle);	
+	}
+	else {
+		message_handle=fopen(argv[4],"w");
+		if (message_handle== NULL) {
+			fprintf(stderr, "Can't open text input file %s\n",argv[4]);
+			exit(1);
+		}
+	
+		/* Grab LSB of all bytes for length specified at fgetc */
+		int message_length=fgetc(file_handle);
+		for(int i=0;i<message_length;i++) {
+			char temp_ch='\0';
+			for( int j=0;j<8;j++) {
+				temp_ch=temp_ch<<1;
+				file_buffer=fgetc(file_handle);
+				int file_byte_lsb = file_buffer & 1; 
+				temp_ch|=file_byte_lsb;
+			}
+			fputc(temp_ch,message_handle);
+		}
+		fclose(message_handle);	
+	}
 
 	/* Clean up before exit */
 	fclose(file_handle);
 	fclose(hidden_message_handle);
-	fclose(message_handle);	
-	printf("\nOffset for image data = %d \nNumber of bytes navigated = %d\n",offset,c);
 }
